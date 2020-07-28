b7d42cd77cb75465029ee31f3a286a7556dffa8e
Jyles Coad-Ward <jyles@dariox.club>
2020-07-28 19:20:30 +0800

    fix

--- modules/developer_alerts/index.js
+++ modules/developer_alerts/index.js
@@ -9,7 +9,7 @@ function addMsgInfo(message) {
     return content.addField("Message Info",`***Author's User Snowflake:*** ${message.author.id}\n***Author:*** <@${message.author.id}>\n***Guild Snowflake:*** ${message.guild.id}\n***Guild Name:*** ${message.guild.name}\n***Channel Name:*** ${message.channel.name}\n***Channel Snowflake:*** ${message.channel.id}`)
 }
 
-// Developer Error 
+// Developer Error
 module.exports.developerError = async function(message,error) {
     if (message.author.bot) return;
     addMsgInfo(message)
@@ -20,7 +20,7 @@ module.exports.developerError = async function(message,error) {
     SB_Client.channels.cache.get(channelJSON.developer.error).send(tmp);
     delete(tmp);
 }
-// Userspace Error 
+// Userspace Error
 module.exports.userspaceError  = async function(message,error) {
     if (message.author.bot) return;
     addMsgInfo(message)
@@ -51,7 +51,7 @@ module.exports.developerAlert = async function (message,error) {
 }
 
 // Developer Unauthorized Access (user that is not in the config attempted to load a command.)
-module.exports.developerUnauth = async function (message,error) {
+module.exports.developerUnauth = async function (message) {
         // Tell the user they're a dumb cunt.
     SB_Client.on('message', async message => {
         let invalidAuthor = new Discord.MessageEmbed()
