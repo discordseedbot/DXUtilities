d3eff930a1391f41ffdd828e91b70026f2ae58d9
Jyles Coad-Ward <jyles@dariox.club>
2020-07-28 19:20:15 +0800

    archive created, tested, and working

---
+++ modules/DX_archive/index.js
@@ -0,0 +1,87 @@
+const Discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
+const prefix = SB_CoreLibrary.prefix().default;
+
+module.exports = async function() {
+
+    SB_Client.on('message', async message => {
+        if (message.author.bot) return;
+        if (message.content.indexOf(prefix) !== 0) return;
+        var args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+
+		var prefrences = SB_Prefrences.archive
+
+        try {
+            switch (command) {
+                case "archive":
+
+
+
+					var allow = {"validGuild": false,"validSelectedChannel":false,"allowUser":false,"categoryExists":false};
+					if (prefrences.allowed_users.indexOf(message.author.id) < -1) {
+						// user not allowed
+						SB_Libraries.forEach(async (m) => {
+							if (m.name === "developer_alerts") {
+								require(`./../../${m.location}/${m.main}`).developerUnauth(message);
+								allow.allowUser = false;
+							}
+						})
+					} else {
+						if (args[0] === undefined) {
+							//channel to archive is not defined
+							message.reply("Channel was not defined, try `"+prefix+command+" #channel`");
+							break;
+						} else {allow.validSelectedChannel=true;}
+
+
+
+						// check if the server is defined in prefrences
+						prefrences.guilds.forEach(async (g) => {
+							if (g.id = message.guild.id) {
+								console.log("guild correct");
+								allow.validGuild = true;
+								if (allow.validSelectedChannel) {
+									console.log("channel valid")
+									var channelIDToMove = args[0].substring(2)
+									var channelIDToMove = channelIDToMove.substring(0, channelIDToMove.length -1);
+									if (allow.validGuild) {
+										message.guild.channels.cache.forEach(async (c) => {
+											if (c.type === "category" && c.id === g.archive_channel) {
+												console.log(channelIDToMove)
+												SB_Client.channels.cache.get(channelIDToMove).setParent(c.id, { lockPermissions: true, reason: `Archived By ${message.author.id}` })
+													.then(channel => {
+														let embed = new Discord.MessageEmbed()
+															.setTitle("Channel Archived")
+															.setDescription("This channel no longer has a purpose but it is locked for archival purposes.\n\n" + `Archived by <@${message.author.id}>`)
+															.setTimestamp()
+														SB_Client.channels.cache.get(channel.id).send(embed);
+														message.author.send(`Channel \`<#${channelIDToMove}>\` has been archived`);
+													})
+											}
+										})
+									}
+								}
+							}
+						})
+					}
+
+
+                    break;
+            }
+        } catch (err) {
+			SB_Libraries.forEach(async (m) => {
+				if (m.name === "developer_alerts") {
+					require(`./../../${m.location}/${m.main}`).userspaceError(message, err);
+					console.error(err);
+				}
+			})
+        }
+
+    })
+
+
+    SB_Client.on('ready', async () => {
+        botModuleConsole.loaded("Archive");
+    })
+}
