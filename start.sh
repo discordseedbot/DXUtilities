494c12c52111576caf87b5ecb1d16fcbdaee3252
Jyles Coad-Ward <jyles@dariox.club>
2020-09-29 19:19:17 +0800

    output log to folder with tee and sed

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /seedbot/DXUtil
-npm start > /seedbot/stable.log 2>&1
+
+while true;
+do
+	npm start | tee "log/$(date +%s).log" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"
+done
+
+#npm start > /seedbot/stable.log 2>&1
