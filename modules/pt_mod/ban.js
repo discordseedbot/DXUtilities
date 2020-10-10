d68fd2f81006c5ae75ad1a03dc4e11ca83d5d30d
Jyles Coad-Ward <jyles@dariox.club>
2020-10-10 17:59:29 +0800

    bugfix

--- modules/pt_mod/ban.js
+++ modules/pt_mod/ban.js
@@ -3,7 +3,7 @@ const { RichEmbed } = require("discord.js");
 
 module.exports = function(message,override) {
 	var args = message.content.slice(SB.prefix.default.length).trim().split( / +/g);
-	let userToBan = message.mentions.users.first() || message.guild.members.cache.get(args[1]).user;
+	let userToBan = message.mentions.users.first() || SB.client.users.cache.get(args[1]);
 	var reason = "";
 	args.forEach((ag)=>{
 		switch (ag) {
@@ -20,7 +20,7 @@ module.exports = function(message,override) {
 	if (reason.length < 1) { message.reply('You must supply a reason for the ban.'); return false; }
 	if (userToBan === undefined) { message.reply('You must mention someone to ban them.'); return false; }
 
-	if (!message.guild.member(userToBan).bannable){ message.reply('I cannot ban that member'); return false; };
+	if (message.guild.member(userToBan) !== null && !message.guild.member(userToBan).bannable){ message.reply('I cannot ban that member'); return false; };
 	if (!override) {
 		if (!message.member.hasPermission('BAN_MEMBERS')) {
 			return message.reply('You do not have permissions to ban.');
@@ -28,14 +28,15 @@ module.exports = function(message,override) {
 	}
 
 	if (!message.guild.me.hasPermission('BAN_MEMBERS')) {message.reply("I don't have permission to ban!"); return false;}
-	message.guild.member(userToBan).ban({reason: banReason}).then((member) => {
+	console.log(userToBan)
+	message.guild.members.ban(userToBan,{reason: banReason}).then((member) => {
 		bannedUserID = userToBan.id;
 
 		message.channel.send({embed: {
 			color: 770000,
 			author: {name:`User was banned from ${message.guild.name}`},
 			fields: [{
-				name: `Reason // ${member.displayName} was banned`,
+				name: `Reason // ${userToBan.username} was banned`,
 				value: `${reason}`
 			}],
 			timestamp: `Banned at; ${new Date()}`,
@@ -47,7 +48,7 @@ module.exports = function(message,override) {
 			color: 770000,
 			author: {name:`You were banned from ${message.guild.name}`},
 			fields: [{
-				name: `Reason // ${member.displayName} banned`,
+				name: `Reason // ${userToBan.username} banned`,
 				value: `${reason}`
 			}],
 			timestamp: `Banned at; ${new Date()}`,
